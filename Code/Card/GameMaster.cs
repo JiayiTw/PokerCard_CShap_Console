c1fc1f09ee1f764631df82b86dd9fc8aab83a14a
Jiayi <k9184444@gmail.com>
2022-01-14 21:48:42 +0800

    Update GM 增加發牌功能

--- Code/Card/GameMaster.cs
+++ Code/Card/GameMaster.cs
@@ -19,23 +19,50 @@ namespace Poker.Code.Card
         /// <summary>
         /// 電腦玩家
         /// </summary>
-        Player aiPlayer;
+        Player otherPlayer;
+
+        Deck mainDeck;
 
         public GameMaster(Player mainPlayer, Player ai) {
             // set player
             this.mainPlayer = mainPlayer;
-            aiPlayer = ai;
+            otherPlayer = ai;
             InitializeGame();
         }
         private void InitializeGame() {
+            //初始化回合
             CurrentRound = 0;
+            // 初始化牌組
+            mainDeck = new Deck();
         }
-        public void Comparer(Card card1,Card card2) {
-           
+        public void Comparer(Card card1, Card card2) {
+
         }
-        public void Deal() { 
-        
-        
+        /// <summary>
+        /// 發牌給雙方
+        /// </summary>
+        public void Deal() {
+            //幫玩家發牌
+            SetHandToPlayer(mainPlayer);
+            //幫電腦發牌
+            SetHandToPlayer(otherPlayer);
+        }
+        private void SetHandToPlayer(Player player) {
+
+            var _newHand = new Hand();
+            const int _visableCount = 2;
+            const int _hiddenCount = 3;
+            // 抽兩張牌到隱藏
+            for (int i = 0; i < _visableCount; i++)
+            {
+                _newHand.visableCards.Add(mainDeck.Drawcard());
+            }
+            // 抽三張牌到隱藏           
+            for (int i = 0; i < _hiddenCount; i++)
+            {
+                _newHand.hiddinCards.Add(mainDeck.Drawcard());
+            }
+            player.handDeck = _newHand;
         }
     }
 }
