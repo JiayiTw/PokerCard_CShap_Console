80acda1ef4a5586984bc5877041db15328a3ee02
Jiayi <k9184444@gmail.com>
2022-01-14 21:55:27 +0800

    Update Card 新增定義

--- Code/Card/Card.cs
+++ Code/Card/Card.cs
@@ -7,6 +7,16 @@ namespace Poker.Code.Card
     class Card : CardBase
     {
 
+        /// <summary>
+        /// Return true if U R BiggerRRRR
+        /// </summary>
+        /// <param name="card">要比較的卡</param>
+        /// <returns>大，有夠大</returns>
+        public bool IsBiggerThan(Card card) {
+            //還沒時做
+            return false;
+
+        }
 
     }
 }
