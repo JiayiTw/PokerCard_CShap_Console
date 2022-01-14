c5a76ac99abd0b479a1520fefc011ea6b682064f
Jiayi <k9184444@gmail.com>
2022-01-14 22:30:28 +0800

    Creat 全端血汗仔

---
+++ Code/Card/ConsolePrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker.Code.Card
+{
+    class ConsolePrinter
+    {
+        /// <summary>
+        /// 用ConsoleLine Print 雙方的牌在畫面上
+        /// </summary>
+        void ShowBothHand() {
+
+        }
+    }
+}
